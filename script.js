c53111657537d759d5a6f567705e391dbce95e4e
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-06-03 20:24:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 //your JS code here. If required.
  var colorSelect = document.getElementById('colorSelect');
+  let button=document.getElementByTag('input')
+button.addEventListener("click",()=>{
+	var selectedIndex = colorSelect.selectedIndex;
+	 if (selectedIndex !== -1) {
+                // Remove the selected option
+                colorSelect.remove(selectedIndex);
+            }
+})
 
+/*
 colorSelect.addEventListener("click",(event)=>{
 	 var selectedIndex = colorSelect.selectedIndex;
             
