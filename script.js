1fe97d956efe99702565b462ba0f9069252765b2
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-06-03 20:46:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //your JS code here. If required.
- var colorSelect = document.getElementById('colorSelect');
-var button = document.getElementsByTagName('input')[0];
-button.addEventListener("click", () => {
-    colorSelect.remove(colorSelect.selectedIndex);
-});
+  var colorSelect = document.getElementById('colorSelect');
+        var button = document.querySelector('input[type="button"]');
+        button.addEventListener("click", () => {
+            if (colorSelect.selectedIndex !== -1) {
+                colorSelect.remove(colorSelect.selectedIndex);
+            }
+        });
 
 /*
 colorSelect.addEventListener("click",(event)=>{
